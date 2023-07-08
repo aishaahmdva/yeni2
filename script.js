d5b17461700dccd7e32fd6762a67b5f0f382dde8
aisha <axmdvaisha@gmail.com>
2023-07-08 11:35:13 +0400

    duz

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let a =+prompt ('Duzbucaqlinin birinci terefini qeyd edin:');
+let b =+prompt ('Duzbucaqlinin ikinci terefini qeyd edin:');
+
+let sahesi = a*b;
+
+document.getElementById('aisha').innerHTML = 'birinci terefi ' + a + ' sm olan duzbucaqlinin, ikinci terefi ' + b + ' sm olarsa, bu duzbucaqlinin sahesi ' + sahesi + ' sm olacaq.'
+
+
+
+
